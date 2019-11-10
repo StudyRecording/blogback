25cdf491f56d02fcd75087231be4a6cd8de494c8
StudyRecording <30993845+StudyRecording@users.noreply.github.com>
2019-11-10 18:08:29 +0800

    前端页面修改

--- src/main/java/com/hu/blogback/controller/IndexController.java
+++ src/main/java/com/hu/blogback/controller/IndexController.java
@@ -2,17 +2,21 @@ package com.hu.blogback.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name) {
+    @GetMapping("/")
+    public String index() {
 
-//        System.out.println("id: " + id + " name: " + name);
-//        int i = 4/0;
         return "index";
     }
+
+    @GetMapping("/blog")
+    public String blog() {
+
+        return "blog";
+    }
+
 
 }
