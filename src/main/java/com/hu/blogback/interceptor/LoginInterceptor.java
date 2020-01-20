024b4b128b243cb0bd320234ed0b6f4e443aeac2
StudyRecording <30993845+StudyRecording@users.noreply.github.com>
2020-01-20 17:12:03 +0800

    修改拦截器的bug

--- src/main/java/com/hu/blogback/interceptor/LoginInterceptor.java
+++ src/main/java/com/hu/blogback/interceptor/LoginInterceptor.java
@@ -13,14 +13,18 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
                              HttpServletResponse response,
                              Object handler) throws Exception {
 
+        String requestURI = request.getRequestURI();
         HttpSession session = request.getSession();
         if (session.getAttribute("usr")  != null || session.getAttribute("user") != null) {
             return true;
-        } else if (request.getSession().getAttribute("user") == null) {
+        } else if (requestURI.contains("admin")) {
             response.sendRedirect("/admin");
             return false;
-        } else {
+        } else if (requestURI.contains("usr")){
             response.sendRedirect("/usr");
+            return false;
+        } else {
+            response.sendRedirect("/");
             return false;
         }
 
